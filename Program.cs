ee34a83c1d1e308ee31f84d55308a47e5bf695d9
Koelle1 <72825531+Koelle1@users.noreply.github.com>
2020-10-18 04:05:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteuerBerechnung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Daten
+            
+            double Preis = 0.0;
+            double Steuersatz = 0.0;
+            string test = string.Empty;
+            double erg = Preis + Steuersatz;
+
+            //
+
+            Console.WriteLine("\t Preis eingeben: ");
+            Console.WriteLine("");
+            Console.Write("Preis: ");
+            Preis = double.Parse(Console.ReadLine());
+        
+            Console.WriteLine("");
+
+            Console.WriteLine("\t Steuersatz eingeben: ");
+            Console.WriteLine("");
+            Console.Write("Steuersatz: ");
+            Steuersatz = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Ergebnis: {Steuersatz} + { Preis} {erg} ");
+
+
+            
+
+
+        }
+    }
+}
